1479071dcc3a06c7513d22b53fdaf96b61b10a7a
vishalvenkat12 <vishalvenkat1206@gmail.com>
2023-03-14 12:48:35 +0530

    url with /home

--- server.js
+++ server.js
@@ -1,5 +1,12 @@
 const http = require('http');
 const server = http.createServer((req,res)=>{
-    console.log('Vishal');
+    const url = req.url;
+    if (url === '/home') {
+    res.write('<html>');
+    res.write('<head><title>My First Page</title></head>');
+    res.write('<body><h1>Welcome home</h1></body>');
+    res.write('</html>');
+    return res.end();
+    }
 });
 server.listen(4000);
